4a63d6caaa920276926a3e08f9220269664fafe4
Anton Gordeyev <anton.gordeyev@decimadigital.com>
2022-07-25 17:11:05 +0300

    added custom hooks, code refactoring

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React, {useMemo, useState} from 'react';
+import React, {useState} from 'react';
 import '../src/styles/App.scss'
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/modal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
+import {usePosts} from "./hooks/usePosts";
 
 function App() {
     const [posts, setPosts] = useState([]);
     const [filter, setFilter] = useState({sort: '', query: ''});
     const [modal, setModal] = useState(false);
+    const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost]);
@@ -19,16 +21,6 @@ function App() {
     const removePost = (post) => {
         setPosts(posts.filter(p => p.id !== post.id));
     }
-
-    const sortedPosts = useMemo(() => {
-        return filter.sort
-            ? [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
-            : posts;
-    }, [filter.sort, posts]);
-
-    const sortedAndSearchedPosts = useMemo(() => {
-        return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()))
-    }, [filter.query, sortedPosts]);
 
   return (
     <div className="App">
